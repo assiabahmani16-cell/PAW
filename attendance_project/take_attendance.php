907927a5f8d5966f67e9e25429c83b5b67bf780d
assiabahmani96 <assia.bahmani16@gmail.com>
2025-11-28 22:21:01 +0100

    Update Project Files

--- attendance_project/take_attendance.php
+++ attendance_project/take_attendance.php
@@ -2,15 +2,15 @@
 $date = date("Y-m-d");
 $filename = "attendance_$date.json";
 
-// إذا الملف موجود → يعني حضور اليوم تم تسجيله
+
 if (file_exists($filename)) {
     die("Attendance for today has already been taken.");
 }
 
-// تحميل قائمة الطلبة
+
 $students = json_decode(file_get_contents("students.json"), true);
 
-// إدخال الحضور
+
 $attendance = [];
 foreach ($students as $s) {
     $status = $_POST["status_" . $s["student_id"]];
@@ -20,7 +20,7 @@ foreach ($students as $s) {
     ];
 }
 
-// حفظ الملف
+
 file_put_contents($filename, json_encode($attendance, JSON_PRETTY_PRINT));
 
 echo "Attendance saved successfully!";
